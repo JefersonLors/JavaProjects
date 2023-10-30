6ad2789b57e31b9ff9f87ca8906401138200ee35
Jeferson Lors <lors.jeferson@gmail.com>
2023-10-30 19:12:24 -0300

    Atualização dos arquivos

---
+++ src/exceptions/DataNascimentoInvalidaException.java
@@ -0,0 +1,10 @@
+package exceptions;
+
+public class DataNascimentoInvalidaException extends Exception {
+
+	public DataNascimentoInvalidaException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
---
+++ src/exceptions/SexoInvalidoException.java
@@ -0,0 +1,10 @@
+package exceptions;
+
+public class SexoInvalidoException extends Exception {
+
+	public SexoInvalidoException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
