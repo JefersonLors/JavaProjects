380272ec3e046f26f77fe1dc13c9e3b8eb30b4c7
Jeferson Lors <lors.jeferson@gmail.com>
2023-11-19 01:01:44 -0300

    Criação da classe de conexão do projeto

--- Biblioteca/src/Main.java
+++ Biblioteca/src/Main.java
@@ -1,5 +1,21 @@
+import dao.PostgresConnection;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        Main obj = new Main();
+        obj.teste();
         System.out.println("Hello world!");
+    }
+    public void teste() throws SQLException {
+        var conn = new PostgresConnection().getConnection();
+        PreparedStatement ps = conn.prepareStatement( "SELECT * FROM autor");
+        ResultSet rs = ps.executeQuery();
+
+        rs.next();
+        System.out.println(rs);
     }
 }
---
+++ Biblioteca/src/dao/PostgresConnection.java
@@ -0,0 +1,19 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class PostgresConnection {
+    private String url = "jdbc:postgresql://localhost/Biblioteca";
+    private String user = "postgres";
+    private String password = "Jeff$_0301_";
+    private static Connection conn;
+
+    public PostgresConnection() throws SQLException {
+        conn = DriverManager.getConnection(url, user, password);
+    }
+    public static Connection getConnection(){
+        return conn;
+    }
+}
